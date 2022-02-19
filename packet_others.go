b33ba3a66b824b86466900b4db8b441c98b22faa
Matt Layher <mdlayher@gmail.com>
2022-02-19 13:38:24 -0500

    packet: fix macOS build
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- packet_others.go
+++ packet_others.go
@@ -8,6 +8,8 @@ import (
 	"net"
 	"os"
 	"runtime"
+
+	"golang.org/x/sys/unix"
 )
 
 // errUnimplemented is returned by all functions on non-Linux platforms.
@@ -15,3 +17,8 @@ var errUnimplemented = fmt.Errorf("packet: not implemented on %s", runtime.GOOS)
 
 func fileConn(_ *os.File) (*Conn, error)                               { return nil, errUnimplemented }
 func listen(_ *net.Interface, _ Type, _ int, _ *Config) (*Conn, error) { return nil, errUnimplemented }
+
+func fromSockaddr(_ unix.Sockaddr) *Addr { return nil }
+func toSockaddr(_ string, _ net.Addr, _ int, _ uint16) (unix.Sockaddr, error) {
+	return nil, errUnimplemented
+}
